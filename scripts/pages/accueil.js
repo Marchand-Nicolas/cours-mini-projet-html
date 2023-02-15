df189d9dc01444646012c58063c41d2c12709089
Nico <60229704+nicolasito1411@users.noreply.github.com>
2023-02-15 08:54:52 +0100

    first commit

---
+++ scripts/pages/accueil.js
@@ -0,0 +1,33 @@
+const articles = [
+    {
+        titre: "Les Fractales: Kesquecé?",
+        description: "Une figure fractale est un objet mathématique qui présente une structure similaire à toutes les échelles. C'est un objet géométrique « infiniment morcelé » dont des détails sont observables à une échelle arbitrairement choisie. En zoomant sur une partie de la figure, il est possible de retrouver toute la figure ; on dit alors qu’elle est « auto similaire ».",
+        page: "def_fractale"
+    },
+    {
+        titre: "Les Fractales: Histoire",
+        description: "De nombreux exemples de fractales, comme le flocon de Koch ou le tapis de Serpiński ont été découverts à la fin du 19e siècle, mais c'est Benoît Mandelbrot qui, en 1975, a attiré l'attention sur ces objets et leur omniprésence dans la nature ...",
+        page: "hist_fractale"
+    },
+    {
+        titre: "Les Fractales: Dans la nature",
+        description: "Des formes fractales approximatives sont facilement observables dans la nature. Ces objets ont une structure autosimilaire sur une échelle étendue, mais finie : les nuages, les flocons de neige, les montagnes, les réseaux de rivières, le chou-fleur ou le brocoli, et les vaisseaux sanguins.",
+        page: "nat_fractale"
+    }
+]
+
+const container = document.getElementById('articles')
+
+for (let index = 0; index < articles.length; index++) {
+    const element = articles[index];
+    container.innerHTML += `
+    <article style="background-image: url('images/articles/${element.page}.jpg')">
+        <a href="articles/${element.page}.html">
+            <h1>${element.titre}</h1>
+            <p>
+                ${element.description}
+            </p>
+        </a>
+    </article>
+    `
+}
---
+++ scripts/relou.js
@@ -0,0 +1,28 @@
+const relou = document.getElementById('relou');
+const width = 180;
+const height = 30;
+
+const marge = 40;
+
+relou.style.width = `${width}px`;
+relou.style.height = `${height}px`;
+
+const move = (e) => {
+    const pageWidth = window.innerWidth;
+    const pageHeight = window.innerHeight;
+    const x = e.clientX;
+    const y = e.clientY;
+    const rect = relou.getBoundingClientRect();
+    const left = rect.left;
+    const top = rect.top;
+
+    // Détécter quand le souris s'approche du bouton, avec une marge de 'marge' pixels
+    if (x > left - marge && x < left + width + marge && y > top - marge && y < top + height + marge) {
+        const newX = Math.floor(Math.random() * (pageWidth - width));
+        const newY = Math.floor(Math.random() * (pageHeight - height));
+        relou.style.left = `${newX}px`;
+        relou.style.top = `${newY}px`;
+    }
+}
+
+document.addEventListener('mousemove', move);
