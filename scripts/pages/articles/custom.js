3bd4fa73716d919de5bff6f284d7304a82a4d97b
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-02-17 14:31:20 +0100

    Adding a comment section

--- scripts/pages/articles/custom.js
+++ scripts/pages/articles/custom.js
@@ -14,3 +14,44 @@ fetch(`${config.api}/getArticle?id=${articleId}`, requestOptions)
     document.getElementById('container').style.backgroundImage = `url(${config.api}/image/${data.image})`;
     screen.remove();
 });
+
+const chargerCommentaires = () => {
+  const commentairesContainer = document.getElementById('commentaires');
+  const requestOptions = {
+    method: 'GET',
+  };
+  fetch(`${config.api}/getCommentaires?articleId=${articleId}`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      commentairesContainer.innerHTML = '';
+      for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        commentairesContainer.innerHTML += `
+        <div class="commentaire">
+          <div class="flex">
+            <h1>${element.auteur}</h1>
+            <p class="ml-1">${new Date(element.date).toLocaleDateString()}</p>
+          </div>
+          <p>${element.contenu}</p>
+        </div>
+        `;
+      }
+    });
+};
+chargerCommentaires();
+
+const ajouterCommentaire = (e) => {
+  e.preventDefault();
+  const auteur = document.getElementById('formCommentaireAuteur').value;
+  const contenu = document.getElementById('formCommentaireContenu').value;
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ auteur: auteur, contenu: contenu, articleId: articleId }),
+  };
+  fetch(`${config.api}/addCommentaire`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      chargerCommentaires();
+    });
+};
